42a6b3ca1224bcf3c3a8db0a8b4bba669081a745
smailnajim <smailnajim>
2025-02-09 14:02:08 +0100

    edit how to affich pages to layot

--- App/Controllers/Front/HomeController.php
+++ App/Controllers/Front/HomeController.php
@@ -2,7 +2,18 @@
 namespace App\Controllers\Front;
 
 class HomeController {
+
     public function index() {
+        $this->layotHead();
         include_once "./../App/View/Home.php";
+        $this->layotFoter();
+    }
+
+    private function layotFoter(){
+        include_once "./../App/View/Layot/Foter.twig";
+    }
+
+    private function layotHead(){
+        include_once "./../App/View/Layot/Head.twig";
     }
 }
